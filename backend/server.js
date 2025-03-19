cfe773913cf3b1ca58ebbad01152dcdc48680d16
NKSG100 <nksnamannks@gmail.com>
2025-03-20 03:35:20 +0530

    Added CORS

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors(
+  {
+    origin: process.env.FRONTEND_URI,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    credentials: true 
+  }
+));
 app.use(express.json());
 app.use(limiter);
 
